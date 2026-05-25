6af6cbe100d415932886e8d8e79eab9d938c6e1f
Fernando Pinho <fernandoalbertopinho20@gmail.com>
2026-05-25 14:54:14 +0100

    First Steps in Java

---
+++ rest-with-spring-boot-and-java-erudio/src/main/java/com/fernandopinho17/model/Greeting.java
@@ -0,0 +1,4 @@
+package com.fernandopinho17.model;
+
+public record Greeting(long id, String content) {
+}
---
+++ rest-with-spring-boot-and-java-erudio/src/test/java/com/fernandopinho17/StartupTests.java
@@ -0,0 +1,13 @@
+package com.fernandopinho17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
